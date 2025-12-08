c8305427a753a9c98b7ba623d82ef5da704247cd
AmbientPush <moscickibartek5@gmail.com>
2025-12-08 20:11:09 +0100

    h3

--- src/apl/AutomatonDynamicsMod.java
+++ src/apl/AutomatonDynamicsMod.java
@@ -1,17 +1,18 @@
 package apl;
 
 import apl.blocks.*;
+import apl.other.*;
 import arc.util.*;
 import mindustry.mod.*;
 
 public class AutomatonDynamicsMod extends Mod {
     public AutomatonDynamicsMod() {
-        ClassMap.classes.put("APLLimitedBattery", adl.blocks.LimitedBattery.class);
-        ClassMap.classes.put("APLHeatConductorDamage", adl.blocks.HeatConductorDamage.class);
-//        ClassMap.classes.put("APLHeadacheCrafter", adl.blocks.HeadacheCrafter.class);
-//        ClassMap.classes.put("APLFactoryPlan", adl.other.FactoryPlan.class);
-//        ClassMap.classes.put("APLPlanDrawer", adl.other.PlanDrawer.class);
-        ClassMap.classes.put("APLVeryStaticWall", adl.blocks.VeryStaticWall.class);
+        ClassMap.classes.put("APLLimitedBattery", apl.blocks.LimitedBattery.class);
+        ClassMap.classes.put("APLHeatConductorDamage", apl.blocks.HeatConductorDamage.class);
+//        ClassMap.classes.put("APLHeadacheCrafter", apl.blocks.HeadacheCrafter.class);
+//        ClassMap.classes.put("APLFactoryPlan", apl.other.FactoryPlan.class);
+//        ClassMap.classes.put("APLPlanDrawer", apl.other.PlanDrawer.class);
+        ClassMap.classes.put("APLVeryStaticWall", apl.blocks.VeryStaticWall.class);
 
     }
 
