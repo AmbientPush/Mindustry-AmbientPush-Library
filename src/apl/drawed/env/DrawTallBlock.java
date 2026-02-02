cac1f576aa76d9e8bf8b66dcbeae61661bbf2e9c
AmbientPush <>
2026-02-02 12:43:10 +0100

    h5

--- src/apl/drawed/env/DrawTallBlock.java
+++ src/apl/drawed/env/DrawTallBlock.java
@@ -5,13 +5,19 @@ import arc.graphics.g2d.*;
 import arc.math.*;
 import mindustry.graphics.*;
 import mindustry.world.*;
-import mindustry.world.blocks.environment.TallBlock;
+import mindustry.world.blocks.environment.*;
 import mindustry.world.draw.*;
 
 public class DrawTallBlock extends TallBlock{
-    public DrawBlock drawer = new DrawDefault();
-
+    public @Nullable DrawBlock drawer;
     public DrawTallBlock(String name){
         super(name);
+    }
+
+    public class DrawTallBlockBuild extends TallBlockBuild{
+        @Override
+        public void draw(){
+            drawer.draw(this);
+        }
     }
 }
